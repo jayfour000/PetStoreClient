288b74fbb06208409d1fc8eae556e41addc2f08f
Jason_Hanson <jayfour000@gmail.com>
2018-03-28 16:12:18 -0500

    Initial commit

---
+++ PetStoreClient/PetStoreClient.h
@@ -0,0 +1,19 @@
+//
+//  PetStoreClient.h
+//  PetStoreClient
+//
+//  Created by Jason Hanson on 3/28/18.
+//  Copyright © 2018 Client Resources, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PetStoreClient.
+FOUNDATION_EXPORT double PetStoreClientVersionNumber;
+
+//! Project version string for PetStoreClient.
+FOUNDATION_EXPORT const unsigned char PetStoreClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PetStoreClient/PublicHeader.h>
+
+
